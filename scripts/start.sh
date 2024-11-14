8b922586a882f1d14e9aa418e2c9d1f770d68ba5
StinkyTACO <35038928+StinkyTACO@users.noreply.github.com>
2024-11-14 21:21:51 +0100

    Create start.sh

---
+++ scripts/start.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+mkdir -p "${STEAMAPPDIR}" || true
+
+# Download Updates
+
+bash "${STEAMCMDDIR}/steamcmd.sh" +force_install_dir "${STEAMAPPDIR}" \
+				+login "${STEAMUSER}" \
+				+app_update "${STEAMAPPID}" \
+				+quit
+# Switch to server directory
+cd "${STEAMAPPDIR}"
+
+# Start Server
+./server --port "${PA_PORT}" \
+--headless \
+--allow-lan \
+--mt-enabled \
+--max-players "${PA_MAXPLAYERS}" \
+--max-spectators 5 \
+--community-servers-url auto \
+--http \
+--spectators 5 \
+--server-password "${PA_PW}" \
+--empty-timeout 5 \
+--replay-filename "UTCTIMESTAMP" \
+--replay-timeout 180 \
+--gameover-timeout 360 \
+--server-name "${PA_SERVERNAME}" \
+--game-mode "PAExpansion1:config" \
+--output-dir "${STEAMAPPDIR}/logs"
